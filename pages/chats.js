3168dd0f56286221f5cee9fdf2b3f0cdbd37166e
Mahalegaurav <mahalegauravk@gmail.com>
2025-12-17 21:42:28 +0530

    fix: add complete bottom navigation to chats page
    - Add all 4 navigation items: Home, Chats, Create, Sign In
    - Matches homepage bottom navigation bar
    - Chats item highlighted in black as active page
    - Create button links to /personas page

--- pages/chats.js
+++ pages/chats.js
@@ -116,9 +116,10 @@ export default function ChatsPage() {
                     )}
                 </main>
 
-                {/* Simple Bottom Navigation */}
+                {/* Bottom Navigation - Matches Homepage */}
                 <nav className="md:hidden fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 z-50">
                     <div className="flex justify-around items-center h-16 px-2">
+                        {/* Home */}
                         <Link
                             href="/"
                             className="flex flex-col items-center justify-center flex-1 py-2 text-gray-400 hover:text-gray-600"
@@ -129,6 +130,7 @@ export default function ChatsPage() {
                             <span className="text-xs mt-1 font-medium">Home</span>
                         </Link>
 
+                        {/* Chats */}
                         <Link
                             href="/chats"
                             className="flex flex-col items-center justify-center flex-1 py-2 text-black"
@@ -137,6 +139,28 @@ export default function ChatsPage() {
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
                             </svg>
                             <span className="text-xs mt-1 font-medium">Chats</span>
+                        </Link>
+
+                        {/* Create - Navigate to personas page */}
+                        <Link
+                            href="/personas"
+                            className="flex flex-col items-center justify-center flex-1 py-2 text-gray-400 hover:text-gray-600"
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
+                            </svg>
+                            <span className="text-xs mt-1 font-medium">Create</span>
+                        </Link>
+
+                        {/* Account / Sign In */}
+                        <Link
+                            href="/auth/signin"
+                            className="flex flex-col items-center justify-center flex-1 py-2 text-gray-400 hover:text-gray-600"
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                            </svg>
+                            <span className="text-xs mt-1 font-medium">Sign In</span>
                         </Link>
                     </div>
                 </nav>
